ca2fdce7133a211aadf3fad9ad20a3dd2d16adc1
John Andrews <johnnydrews@gmail.com>
2014-12-03 08:32:23 -0500

    add modal

--- ChicagoHotelsCodeChallenge/ModalHotelViewController.h
+++ ChicagoHotelsCodeChallenge/ModalHotelViewController.h
@@ -11,4 +11,13 @@
 
 @interface ModalHotelViewController : UIViewController
 @property (strong, nonatomic) Hotel *selectedHotel;
+@property (weak, nonatomic) IBOutlet UIImageView *hotelImage;
+@property (weak, nonatomic) IBOutlet UILabel *name;
+@property (weak, nonatomic) IBOutlet UILabel *starRating;
+@property (weak, nonatomic) IBOutlet UILabel *nightlyRate;
+@property (weak, nonatomic) IBOutlet UILabel *address;
+@property (weak, nonatomic) IBOutlet UILabel *reviewScore;
+@property (weak, nonatomic) IBOutlet UIButton *dismissModalButton;
+- (IBAction)dismissModalButtonPressed:(id)sender;
+
 @end
--- ChicagoHotelsCodeChallenge/ModalHotelViewController.m
+++ ChicagoHotelsCodeChallenge/ModalHotelViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ModalHotelViewController.h"
+#import "UIImageView+WebCache.h"
 
 @interface ModalHotelViewController ()
 
@@ -18,10 +19,37 @@
 {
     [super viewDidLoad];
     
-    NSLog(@"Selected Hotel Name =%@", self.selectedHotel.name);
+    [self getImage];
+    [self setupUI];
+}
 
+- (void)setupUI
+{
+    self.name.text = self.selectedHotel.name;
+    self.starRating.text = self.selectedHotel.starRating;
+    self.nightlyRate.text = [NSString stringWithFormat:@"$%@", self.selectedHotel.nightlyRate];
+    self.reviewScore.text = [self.selectedHotel.reviewScore stringValue];
+    self.address.text = self.selectedHotel.streetAddress;
+    
+    self.dismissModalButton.backgroundColor = [UIColor lightGrayColor];
+    self.dismissModalButton.layer.cornerRadius = 8;
+    self.dismissModalButton.layer.borderWidth = 1;
+    self.dismissModalButton.layer.borderColor = [UIColor blueColor].CGColor;
+    self.dismissModalButton.clipsToBounds = YES;
+    self.dismissModalButton.titleLabel.textColor = [UIColor blueColor];
+}
+
+- (void)getImage
+{
+    [self.hotelImage sd_setImageWithURL:[NSURL URLWithString:self.selectedHotel.thumbnailURL]
+                              completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+                                  self.hotelImage.image = image;
+                              }];
 }
 
 
-
+- (IBAction)dismissModalButtonPressed:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 @end
